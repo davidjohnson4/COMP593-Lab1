c2d1aca4875d631cd276979bf32a6afbe53dcf52
davidjohnson4 <98180484+davidjohnson4@users.noreply.github.com>
2022-05-27 15:49:01 -0400

    modified both files in the repo done
    -deleted statement from script 1
    -added a statement in script 2

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #prints lines from the zen python
 print("Beautiful is better than ugly.")
-print("Namespaces are one honking great idea -- let's do more of those!")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #prints lines from zen of python
 print("Simple is better than complex.")
 print("Although never is often better than *right* now.")
+print("Namespaces are one honking great idea -- let's do more of those!")
